58398bb6a3da353eb3df8cabe9ab8f98aa9ca4eb
corvusnigra <itemruby@gmail.com>
2017-11-07 00:08:21 +0300

    communication between component

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,26 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>Name is {{name}}</p>
+        <p>Name is {{ changeName() }}</p>
+        <button @click="revertName">Revert name</button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['name']
+        props: {
+            name : String
+        },
+        methods: {
+            changeName(){
+                return this.name.split('').reverse().join('')
+            },
+            revertName(){
+                this.name = 'Max';
+                this.$emit('eventReset', this.name);
+            }
+
+        }
     }
 </script>
 
